a9f8e68b16cdc2ec1fd75ed9a6db087fe79d937b
rostyslavmoskal <moskalrostyslav@gmail.com>
2022-09-16 14:36:52 -0700

    spring: inversion of control

---
+++ Spring-01-Core-IoC/src/main/java/com/cydeo/bean_annotation/ConfigAny.java
@@ -0,0 +1,13 @@
+package com.cydeo.bean_annotation;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ConfigAny {
+
+    @Bean
+    String str(){
+        return "Developer";
+    }
+}
---
+++ Spring-01-Core-IoC/src/main/java/com/cydeo/stereotype_annotation/Selenium.java
@@ -0,0 +1,9 @@
+package com.cydeo.stereotype_annotation;
+
+import org.springframework.stereotype.Component;
+
+public class Selenium {
+    public void getTeachingHours() {
+        System.out.println("Total teaching hours: 200");
+    }
+}
