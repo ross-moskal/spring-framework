9933eae8d049a3dade4005999b76d4ab11ee5df7
rostyslavmoskal <moskalrostyslav@gmail.com>
2022-09-16 17:36:31 -0700

    spring: dependency injection

---
+++ Spring-03-Core-DI/src/main/java/com/cydeo/Java.java
@@ -0,0 +1,25 @@
+package com.cydeo;
+
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+@AllArgsConstructor
+public class Java {
+    // Field Injection(deprecated)
+//    @Autowired
+//    OfficeHours officeHours;
+
+    // Constructor injection
+    OfficeHours officeHours;
+
+//    @Autowired
+//    public Java(OfficeHours officeHours) {
+//        this.officeHours = officeHours;
+//    }
+
+    public void getTeachingHours() {
+        System.out.println("Weekly teaching hours: " + (20 + officeHours.getHours()));
+    }
+}
