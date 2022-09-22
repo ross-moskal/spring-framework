924693aaca1aca64f325cee9f082f63cc5cff47a
rostyslavmoskal <moskalrostyslav@gmail.com>
2022-09-21 20:09:46 -0700

    spring: Spring MVC Model added

---
+++ Spring-08-Spring-MVC-Model/src/main/java/com/cydeo/controller/MentorController.java
@@ -0,0 +1,26 @@
+package com.cydeo.controller;
+
+import com.cydeo.enums.Gender;
+import com.cydeo.model.Mentor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+@RequestMapping("/mentor")
+public class MentorController {
+
+    @RequestMapping("/list")
+    public String showTable(Model model) {
+        List<Mentor> mentorList = new ArrayList<>();
+        mentorList.add(new Mentor("Jack", "Ma", 52, Gender.MALE));
+        mentorList.add(new Mentor("Tom", "Hanks", 65, Gender.MALE));
+
+        model.addAttribute("mentors", mentorList);
+
+        return "mentor/list";
+    }
+}
