2c2f2283463b678f9e9d60ed18f37ec78d831234
rostyslavmoskal <moskalrostyslav@gmail.com>
2022-10-23 19:24:13 -0700

    spring: ORM cinemaLab task

---
+++ Spring-13-CinemaLab/src/main/java/com/cydeo/entity/Movie.java
@@ -0,0 +1,42 @@
+package com.cydeo.entity;
+
+import com.cydeo.enums.MovieState;
+import com.cydeo.enums.MovieType;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
+
+@Entity
+@NoArgsConstructor
+@Data
+public class Movie extends BaseEntity{
+    private String name;
+    @Column(columnDefinition = "DATE")
+    private LocalDate releaseDate;
+    private Integer duration;
+    @Column(columnDefinition = "text")
+    private String summary;
+    @Enumerated(EnumType.STRING)
+    private MovieType type;
+    @Enumerated(EnumType.STRING)
+    private MovieState state;
+    private BigDecimal price;
+
+    @ManyToMany
+    @JoinTable(name = "movie_genre_rel", joinColumns = @JoinColumn(name = "movie_id"), inverseJoinColumns = @JoinColumn(name = "genre_id"))
+    private List<Genre> genreList;
+
+    public Movie(String name, LocalDate releaseDate, Integer duration, String summary, MovieType type, MovieState state, BigDecimal price) {
+        this.name = name;
+        this.releaseDate = releaseDate;
+        this.duration = duration;
+        this.summary = summary;
+        this.type = type;
+        this.state = state;
+        this.price = price;
+    }
+}
