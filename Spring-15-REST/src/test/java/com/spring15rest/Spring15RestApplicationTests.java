924be303dca63b8a8dace8870abc8eab9407062b
rostyslavmoskal <moskalrostyslav@gmail.com>
2023-01-07 16:38:39 -0800

    feat: init commit

---
+++ Spring-15-REST/src/test/java/com/spring15rest/Spring15RestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring15rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring15RestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
